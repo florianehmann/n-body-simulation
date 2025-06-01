6fd7ff99f478ae5a17ff5af5d06078711e76f42b
Florian Ehmann <kontakt@florianehmann.de>
2025-06-01 22:02:36 +0200

    Remove leftover debug variable

--- src/sim/universe.rs
+++ src/sim/universe.rs
@@ -209,22 +209,19 @@ impl Universe {
 
         // determine approximate forces
         for particle in &mut self.particles {
-            let mut calls: usize = 0;
             let mut f = |agg: &SubtreeAggregate| {
-                calls += 1;
                 let r_vec = agg.center_of_mass - particle.pos;
 
                 let r_squared = r_vec.norm_squared();
                 let softened = r_squared + 0.01; // (r^2 + epsilon^2)
                 let inv_r = 1.0 / softened.sqrt();
 
-                let f12 = 1.0e-6 * agg.total_mass * inv_r * inv_r;
+                let f12 = 1.0e-4 * agg.total_mass * inv_r * inv_r;
                 let f12_vec = f12 * r_vec * inv_r;
 
                 particle.force += f12_vec;
             };
             tree.for_each_relevant_aggregate(particle.pos, 1.0, &mut f);
-            // println!("Called closure {calls} times.");
         }
 
         // update velocities and positions
